4fa5dfb71745b5ffed797b48e4d802ff606705d9
Sebastian Obregon <oesseba@gmail.com>
2013-04-29 00:56:16 -0300

    Se agrega la carga de los combos de busqueda de forma local.

--- DondeEstoy/src/edu/palermo/dondeestoy/Busqueda.java
+++ DondeEstoy/src/edu/palermo/dondeestoy/Busqueda.java
@@ -4,17 +4,48 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
+import edu.palermo.dondeestoy.services.BusquedaService;
+import edu.palermo.dondeestoy.services.BusquedaServiceImplLocal;
 
 public class Busqueda extends Activity{
 	
 	private Button botonBuscar;
+	
+	
+	private Spinner spinnerCategoria;
+	private Spinner spinnerLugar;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.busqueda_layout);
 		botonBuscar = (Button) findViewById(R.id.buttonBuscar);
+		
+				
+		spinnerCategoria = (Spinner)  findViewById(R.id.spinnerCategoria);
+		spinnerLugar = (Spinner)  findViewById(R.id.spinnerBarrio);
+
+    	BusquedaService busquedaService = new BusquedaServiceImplLocal();
+
+        ArrayAdapter<Categoria> dataAdapterCategoria = new ArrayAdapter<Categoria>(this,
+    		android.R.layout.simple_spinner_item, busquedaService.getCategorias());
+        
+        dataAdapterCategoria.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerCategoria.setAdapter(dataAdapterCategoria);
+        
+    	
+        ArrayAdapter<Lugar> dataAdapterLugar = new ArrayAdapter<Lugar>(this,
+    		android.R.layout.simple_spinner_item, busquedaService.getLugares());
+        
+        dataAdapterLugar.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerLugar.setAdapter(dataAdapterLugar);
+		
+		
+		
+		
 		botonBuscar.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View arg0) {
